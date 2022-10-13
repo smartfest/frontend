a1ec7a3ec10ab08a9edb1044c76fc4542b64a0e1
Dante R. Giuliano <dante.giuliano@est.fi.uncoma.edu.ar>
2022-10-13 18:30:56 -0300

    update

---
+++ app/js/evento.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+    var card = document.getElementById("flyer");
+    var test = document.cookie;
+    card.innerHTML(test)
+});
--- app/js/index.js
+++ app/js/index.js
@@ -5,7 +5,8 @@ document.addEventListener("DOMContentLoaded", (event) => {
 });
 
 function go() {
-    alert("test")
+    document.cookie="Tuki"
+    window.location.href("pages/evento.html")
 }
 
 function create_panel(card) {
@@ -22,6 +23,10 @@ function create_templeate(evento) {
     let ubicacion = evento.ubicacion;
     let fecha = evento.fecha_evento
     return `<div class="card" onclick="go()"> <div class="image" style="background-image: url(${background})"></div><h2>${titulo}</h2><div class="card-content"><h2></h2><h3>Ubicacion: ${ubicacion}</h3><h3>Fecha: ${fecha}</h3></div></div>`
+}
+
+function tuki(){
+    document.cookie="asdoasdnsa"
 }
 
 
