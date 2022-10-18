6ca4636cf2501136dcb4134f7e328969b10d101c
dante <dante.giuliano@est.fi.uncoma.edu.ar>
2022-10-18 19:21:24 -0300

    version estable

--- app/js/evento.js
+++ app/js/evento.js
@@ -19,24 +19,23 @@ function createEvent(id) {
     let data = eventos.filter(evento => { return evento.id == id })[0];
     console.log(data)
     let evento = document.getElementById("evento");
-    evento.innerHTML=template(data)
-    
-    
-}   
+    evento.innerHTML = template(data)
+
+
+}
 
 function template(data) {
-    return `<h1 id="titulo">${data.titulo}fest</h1>
-  <img id="flyer" src="${"../"+data.flyer}" alt="">
-
+    return `<h1 class="titulo">${data.titulo}fest</h1>
+  <img class="flyer" src="${"../" + data.flyer}" alt="Evento "${data.descripcion}>
   <div class="informacion">
-      <div>
-          <h5 id="ubicacion">Ubicacion: ${data.ubicacion}</h5>
-          <h5 id="horario">Horario :${data.horario_inicio}</h5>
-      </div>
-      <div class="redes-sociales">
+  <div>
+    <h5>Ubicacion: ${data.ubicacion}</h5>
+    <h5>Horario :${data.horario_inicio}</h5>
+  </div>
+    <div class="redes-sociales">
           <img src="../../assets/icons/Facebook.svg" alt="">
           <img src="../../assets/icons/Instagram.svg" alt="">
           <img src="../../assets/icons/Twitter.svg" alt="">
-
-      </div>`
+    </div>
+    </div>`
 }
